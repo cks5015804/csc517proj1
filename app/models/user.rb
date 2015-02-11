9951b3160e1e39fa10e667de6b718412c93af0c6
Miguel Rufino <marufino@ncsu.edu>
2015-02-11 17:29:14 -0500

    Added user controller and views Changing user "type" to "role" since type is a reserved keyword in ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
 
   # users can only belong to one project at a time
   # users can only be signed up for one story at a time
-  belongs_to :project, :story
+  belongs_to :project#, :story
 
   validates :email, :presence => true
   validates :password, :presence => true
-  validates :type, :presence => true
+  validates :role, :presence => true
 
 end
--- db/migrate/20150210012334_create_users.rb
+++ db/migrate/20150210012334_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :password
-      t.string :type
+      t.string :role
 
       t.timestamps
     end
