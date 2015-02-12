15744117e658628aa303e0a591c774fa84bf3fc4
ylu31@ncsu.edu <ylu31@ncsu.edu>
2015-02-11 22:18:16 -0500

    changed model relation

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,11 +3,11 @@ class Story < ActiveRecord::Base
   STAGE_OPTIONS = {'Analysis' => 1, 'Ready For Dev' => 2, 'In Dev' => 3,
                     'Dev Complete' => 4, 'In Test' => 5, 'Complete' => 6}
 
-  # categories are part of a project
+  # a story can belong to only 1 project
   belongs_to :project
 
-  # categories can have 0~2 users working on them
-  has_many :user
+  # a story can have 0~2 users working on them
+  has_many :users, dependent: :nullify
 
   validates :title, :presence => true
   validates :description, :presence => true
