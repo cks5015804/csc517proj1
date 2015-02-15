e78b4cfa38a0b8b3f5b17cfe4c238b42271d2e07
ylu31@ncsu.edu <ylu31@ncsu.edu>
2015-02-15 12:12:19 -0500

    Fixed a bug for showing stories. (continued 2)

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -10,7 +10,7 @@ class StoriesController < ApplicationController
       @user = current_user
       if @user.project_id
         #@stories = Story.find([@user.story_id, @user.story_id])
-        @stories = Story.where("project_id == '#{@user.project_id}'")
+        @stories = Story.where("project_id = '#{@user.project_id}'")
         @project = Project.find(@user.project_id)
       end
     else
